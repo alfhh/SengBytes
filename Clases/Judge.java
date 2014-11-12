3bc127965d6616fcede8dac6f1bdc094ea4bd720
Alfredo Hinojosa <alfhh4@gmail.com>
2014-11-11 23:58:50 -0600

    Update Judge.java

--- Clases/Judge.java
+++ Clases/Judge.java
@@ -1,3 +1,23 @@
 class Judge { 
 	int initService;
+
+	  public void aprobarArticulo(Article art, Publication pub){
+	   	try {
+	         String s = "INSERT INTO Articulo (ID, Publicado, IdAutor, IdRevista, NumArt)" +
+	                   " VALUES ("+ art.id + " , '" + art.published + " , '" +
+	                             authorId + " , '" + pub.id + ", '" + art.dateCreation " )"; 
+	         conn.stmt.executeUpdate(s);
+	      }catch (Exception e) { System.out.println ("Cannot update table: Articulo" + e ); }     
+	   }
+   }
+
+     public void rechazarArticulo(Article art){
+	   	try {
+	         String s = "DELETE FROM Registration " +
+                   "WHERE id = " + art.id;
+	         conn.stmt.executeUpdate(s);
+	      }catch (Exception e) { System.out.println ("Cannot update table: Articulo" + e ); }     
+	   }
+   }
 }
+
