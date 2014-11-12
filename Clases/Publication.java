8f599de45ac3602923f473667352807385416e0d
Roel Castano <roelcm@Roels-MacBook-Pro-3.local>
2014-11-11 23:45:11 -0600

    Setter and getter added for Publication

--- Clases/Publication.java
+++ Clases/Publication.java
@@ -1,5 +1,12 @@
-class Publication { 
+class Publication {
    int id;
    int date;
    int numArts;
+
+   public void setDate(int d){
+     date = d;
+   }
+   public int getDate(){
+     return date;
+   }
 }
