a2f0b782d1f99017b4bf725097f6790a4028899b
Roel Castano <roelcm@Roels-MacBook-Pro-3.local>
2014-11-11 23:17:07 -0600

    Add normal methods to Suscriptor

--- Clases/Suscriptor.java
+++ Clases/Suscriptor.java
@@ -1,4 +1,4 @@
-class Suscriptor { 
+class Suscriptor {
    int id;
    String nom;
    String lastN;
@@ -8,4 +8,28 @@ class Suscriptor {
    String mail;
    String password;
    int initSusc;
+
+   public void setAddress(String add){
+     address = add;
+   }
+
+   public String getAddress(){
+     return address;
+   }
+
+   public void changePassword(String oldPass, String newPass){
+     if (oldPass == password){
+       password = newPass;
+     }
+   }
+
+  public void changeMail(String newMail){
+      mail = newMail;
+  }
+
+   public void changeSubscriptionType(int s){
+     if(s>=0 && s <=2){
+       suscriptionType = s;
+     }
+   }
 }
