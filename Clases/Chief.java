0d3ca56240ef64c46f4afcc4e27ec354e42cca44
Roel Castano <roelcm@Roels-MacBook-Pro-3.local>
2014-11-11 23:30:20 -0600

    Adds change Password and changeMail methods

--- Clases/Chief.java
+++ Clases/Chief.java
@@ -1,7 +1,18 @@
-class Chief { 
+class Chief {
    int id;
    String nom;
    String lastN;
    String mail;
    String password;
+
+   public void changePassword(String oldPass, String newPass){
+     if (oldPass == password){
+       password = newPass;
+     }
+   }
+
+  public void changeMail(String newMail){
+      mail = newMail;
+  }
+
 }
